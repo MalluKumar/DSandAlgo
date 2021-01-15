1ca7d9cb57fece1f09a41c981dbce6828f663ce7
MalluMalligereKumar <mallumkumar069@gmail.com>
2021-01-15 14:17:36 +1100

    Add ArrayList basic operations

---
+++ src/Lists/ArrayList/TestPerson.java
@@ -0,0 +1,45 @@
+package Lists.ArrayList;
+
+import Lists.ArrayList.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestPerson {
+
+    public static void main (String[] args) {
+
+        List<Person> persons = new ArrayList<>();
+
+        persons.add(new Person("Virat", "Kohli", 26));
+        persons.add(new Person("Rohit", "Sharma", 24));
+        persons.add(new Person("MS", "Dhoni", 14));
+        persons.add(new Person("KL", "Rahul", 10));
+
+        persons.forEach(person -> System.out.println(person));
+        System.out.println("---------------------------------------------");
+
+        persons.set(3, new Person("Rahul", "Dravid", 44));
+        persons.forEach(person -> {System.out.println(person);});
+        System.out.println("----------------------------------------------");
+
+        Person[] personArray = persons.toArray(new Person[persons.size()]);
+
+        for (Person person : personArray) {
+            System.out.println(person);
+        }
+        System.out.println("-----------------------------------------------");
+
+        persons.remove(0);
+
+        persons.forEach(person -> System.out.println(person));
+        System.out.println("-----------------------------------------------");
+
+        System.out.println(persons.contains(new Person("Rohit", "Sharma", 24)));
+        System.out.println(persons.indexOf(new Person("MS", "Dhoni", 14)));
+
+        System.out.println("------------------------------------------------");
+
+    }
+
+}
